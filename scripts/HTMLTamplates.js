b34da9d2cd48c9e8c490278943a8f803688d170b
D-Aldin <dobricaldin@gmail.com>
2025-01-24 22:43:21 +0100

    create HTMLTamplates.js file and renderCard function

---
+++ scripts/HTMLTamplates.js
@@ -0,0 +1,28 @@
+function renderCard(category, title, discription, subtasks, contact) {
+  return ` 
+              <article class="card" draggable="true">
+                <div class="category">${category}</div>
+                <div class="card_title">
+                  <h4 id="title">${title}</h4>
+                </div>
+                <div class="discription">
+                  <h5 id="discription">${discription}</h5>
+                </div>
+                <div class="progress_indicator">
+                  <div class="progress_container">
+                    <div class="progress_bar" style="width: 20%"></div>
+                  </div>
+                  <span id="subtasks">${subtasks}</span>
+                </div>
+                <div class="profile_prio_container">
+                  <div class="profile">
+                    <div class="circle">${contact}</div>
+                    <div class="circle" style="transform: translateX(-50%); background-color: rgb(31, 215, 193)">DN</div>
+                  </div>
+                  <div class="prio">
+                    <img src="./assets/icons/board/urgent.svg" alt="" />
+                  </div>
+                </div>
+              </article>
+      `;
+}
