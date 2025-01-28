973796a72dba1faa89e5a20ccb5a4418fd5495d3
RobbyR98 <robbyrunge@yahoo.com>
2025-01-28 20:04:06 +0100

    add new overlay by click on button 'Add new contact' by contacts.html

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -3,14 +3,14 @@ function openOverlayBackground() {
   let overlayCardRef = document.getElementById('overlay_add_contact_card');
   overlayRef.classList.add('overlay_background');
   overlayCardRef.classList.add('overlay_add_contact_card');
-  overlayCardRef.style.animation = 'slideInFromRight 0.5s forwards';
+  overlayCardRef.style.animation = 'slideInFromRight 0.3s forwards';
 }
 
 function closeOverlayBackground() {
   let overlayRef = document.getElementById('overlay_add_contacts_background');
   let overlayCardRef = document.getElementById('overlay_add_contact_card');
   overlayRef.style.backgroundColor = 'transparent';
-  overlayCardRef.style.animation = 'slideOutToRight 0.5s forwards';
+  overlayCardRef.style.animation = 'slideOutToRight 0.3s forwards';
   overlayCardRef.addEventListener('animationend', () => {
     overlayRef.classList.remove('overlay_background');
     overlayCardRef.classList.remove('overlay_add_contact_card');
