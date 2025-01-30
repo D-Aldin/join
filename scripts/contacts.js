c5666278999143d6433222540974fc3a84b88c4b
RobbyR98 <robbyrunge@yahoo.com>
2025-01-30 17:13:06 +0100

    add edit/delete overlay, make funktionality of delete inputs, links by help.html updated

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -6,14 +6,48 @@ function openOverlay() {
   overlayCardRef.style.animation = 'slideInFromRightAddContact 0.3s forwards';
 }
 
-function closeOverlay() {
+function openOverlayEditContact() {
+  let overlayRef = document.getElementById('overlay_add_contacts_background');
+  let overlayEditCardRef = document.getElementById('overlay_edit_contact_card');
+  overlayRef.classList.add('overlay_background');
+  overlayEditCardRef.classList.add('overlay_edit_contact_card');
+  overlayEditCardRef.style.animation = 'slideInFromRightAddContact 0.3s forwards';
+}
+
+function deleteInputs() {
+  let inputs = document.querySelectorAll('input');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+  }
+}
+
+// function saveChangesContact() {
+  
+// }
+
+function closeOverlayAddContact() {
   let overlayRef = document.getElementById('overlay_add_contacts_background');
   let overlayCardRef = document.getElementById('overlay_add_contact_card');
+  let inputs = document.querySelectorAll('input');
   overlayRef.style.backgroundColor = 'transparent';
   overlayCardRef.style.animation = 'slideOutToRightAddContact 0.3s forwards';
   overlayCardRef.addEventListener('animationend', () => {
     overlayRef.classList.remove('overlay_background');
     overlayCardRef.classList.remove('overlay_add_contact_card');
+    overlayCardRef.style.animation = '';
+    overlayRef.style.backgroundColor = '';
+    deleteInputs();
+  }, { once: true });
+}
+
+function closeOverlayEditContact() {
+  let overlayRef = document.getElementById('overlay_add_contacts_background');
+  let overlayCardRef = document.getElementById('overlay_edit_contact_card');
+  overlayRef.style.backgroundColor = 'transparent';
+  overlayCardRef.style.animation = 'slideOutToRightAddContact 0.3s forwards';
+  overlayCardRef.addEventListener('animationend', () => {
+    overlayRef.classList.remove('overlay_background');
+    overlayCardRef.classList.remove('overlay_edit_contact_card');
     overlayCardRef.style.animation = '';
     overlayRef.style.backgroundColor = '';
   }, { once: true });
