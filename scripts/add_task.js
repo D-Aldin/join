b0a9b71acdb76076b8a71809591e6614babfd409
Simib92 <simib92@hotmail.ch>
2025-02-02 16:56:32 +0100

    add task mew functions and styling upgrade

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,36 +1,59 @@
 function render() {
-    loadContact();
 }
 
 function loadContact() {
     for (let i = 0; i < contacts.length; i++) {
         const contact = contacts[i];
+        let name = contact.name;
+        let img = contact.img;
         let contactlist = document.getElementById('contact-list');
         contactlist.innerHTML = /*html*/`
             <option class="datalist" value="">
                 <img src="" alt=""><p></p><input type="checkbox">
             </option>
-
         `
     }
 }
 
-let buttons = {
-    urgent: false,
-    medium: false,
-    low: false,
-}
+selectedButton = '';
 
 function setPrio(x) {
+    resetPrioButton()
     let button = document.getElementById(x);
-    if (buttons.y = true) {
-        removePrio(x);
-    } else {
-        button.classList.add('backgroundcolor'+x);
-    buttons.y = true}
-    
+    selectedButton = x;
+    button.classList.add('backgroundcolor'+x);
+    button.classList.remove('urgentbutton');
+    document.getElementById('icon'+x).innerHTML = /*html*/`
+        <img src="assets/icons/addTask/icon_clicket_${x}.svg" alt="">
+    `
 }
 
-function removePrio(x) {
-    document.getElementById(x).classList.remove('backgroundcolor'+x);
+function resetPrioButton() {
+    if (selectedButton == '' == false) {
+        let button = document.getElementById(selectedButton);    
+        button.classList.remove('backgroundcolor'+selectedButton);
+        button.classList.add('urgentbutton');
+        document.getElementById('icon'+selectedButton).innerHTML = /*html*/`
+        <img src="assets/icons/addTask/icon_${selectedButton}.svg" alt="">
+    `
+    }
+}
+
+function creatTask() {
+    let title = document.getElementById('').value;
+    let discription = document.getElementById('').value;
+    let date = document.getElementById('').value;
+    let category = document.getElementById('').value;
+}
+
+function clear() {
+    console.log('hello');
+    
+    resetPrioButton();
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('assigned').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('category').value = '';
+    document.getElementById('subtask').value = '';
 }
