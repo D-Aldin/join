db72087a6bdd710c1b07e4384ca2945e7f4f4ce2
RobbyR98 <robbyrunge@yahoo.com>
2025-02-06 16:49:30 +0100

    new files created for a better overview

---
+++ scripts/contact_template.js
@@ -0,0 +1,85 @@
+function getLetterTemplate(letter) {
+  return `
+    <div class="contacts_list_first_letter">
+      <div class="first_letter">${letter}</div>
+    </div>
+    <div class="line_bottom"></div>
+  `;
+}
+
+function getContactTemplate(contact, index) {
+  return `
+    <div id="contact_${index}" class="contact_list hover_contact_list" onclick="toggleOverlayContactInfos(${index})">
+      <div class="circle" id="circle_${index}"></div>
+      <div class="account_info">
+        <p>${contact.name}</p>
+        <span>${contact.email}</span>
+      </div>
+    </div>
+  `;
+}
+
+function getTemplateOfRenderContacts(contact, index) {
+  return `    
+    <div id="contact_${index}" class="contact_list hover_contact_list" onclick="toggleOverlayContactInfos(${index})">
+      <div class="circle" id="circle_${index}"></div>
+      <div class="account_info">
+        <p>${contact.name}</p>
+        <span>${contact.email}</span>
+      </div>
+    </div>`;
+}
+
+function getTemplateOfContactInfo(contact) {
+  const initials = getInitials(contact.name);
+  return `
+    <div class="overlay_contact_info_header">
+    <div class="circle_contact_img" style="background-color:${contact.color}">${initials}</div>
+    <div>
+      <h2>${contact.name}</h2>
+      <div class="btn_position">
+        <img onclick="openOverlayEditContact('${contact.id}')" class="edit_hover" src="./assets/icons/contacts/edit.svg" alt="edit" />
+        <img onclick="deleteContactFromList('${contact.id}'); setTimeoutDeleteOverlayContact()" class="delete_hover" src="./assets/icons/contacts/delete.svg" alt="delete" />
+      </div>
+    </div>
+  </div>
+  <div class="headline_contact_info">
+    <p>Contact Information</p>
+  </div>
+  <div class="contact_info">
+    <span>Email</span>
+    <p class="email_design">${contact.email}</p>
+    <span>Phone</span>
+    <p class="number_design">${contact.phone}</p>
+  </div>`;
+}
+
+function getTemplateOfContactEdit(contact) {
+  return `
+    <div class="overlay_add_contact_left_side">
+      <div class="overlay_logo_position">
+        <img src="./assets/icons/logo/logo_white.svg" alt="logo" />
+      </div>
+      <div class="overlay_headline_add_contact">
+        <h1>Edit contact</h1>
+        <div class="line_contact"></div>
+      </div>
+    </div>
+    <div class="overlay_add_contact_right_side">
+      <div class="overlay_account_icon_position">
+        <img src="./assets/icons/contacts/account_icon.svg" alt="account_icon" />
+      </div>
+      <div class="overlay_close_btn_position">
+        <img onclick="closeOverlayEditContact()" src="./assets/icons/contacts/Close.svg" alt="close_button" />
+      </div>
+      <form action class="form_input_fields_position" onsubmit="updateContactInDataBase('${contact.id}'); setTimeoutSuccessfullyOverlayEdit(); return false;">
+        <input id="edit_name" type="text" placeholder="Name" value="${contact.name}" required />
+        <input id="edit_email" type="email" placeholder="Email" value="${contact.email}" required />
+        <input id="edit_phone" type="tel" placeholder="Phone" value="${contact.phone}" required />
+        <div class="overlay_btn_position">
+          <button class="btn_delete" onclick="deleteContactFromList('${contact.id}'); setTimeoutDeleteOverlayContact()">Delete <img src="./assets/icons/contacts/Close.svg" class="cancel_icon" alt="Cancel" /></button>
+          <button type="submit" class="btn_save">Save <img src="./assets/icons/contacts/check.svg" alt="check" /></button>
+        </div>
+      </form>
+    </div>`;
+}
--- scripts/contacts.js
+++ scripts/contacts.js
@@ -62,7 +62,6 @@ async function updateContactInDataBase(id) {
   let phone = document.getElementById("edit_phone").value;
   let contact = arrayOfContacts.find((c) => c.id === id);
   let color = contact.color;
-  // console.log(BASE_URL + `contacts/${id}.json`);
   try {
     let response = await fetch(BASE_URL + `contacts/${id}.json`, {
       method: "PATCH",
@@ -99,31 +98,6 @@ async function deleteContactFromList(id) {
   }
 }
 
-function closeOverlayContactInfoAfterDelete() {
-  let overlay = document.getElementById("overlay_contact_infos");
-  overlay.style.animation = "slideOutToRightContactInfos 0.3s forwards";
-  overlay.addEventListener(
-    "animationend",
-    () => {
-      overlay.classList.add("d_none");
-      overlay.style.animation = "";
-    },
-    { once: true }
-  );
-}
-
-function deleteInputs() {
-  let inputs = document.querySelectorAll("input");
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].value = "";
-  }
-}
-
-function getRandomColor() {
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-}
-
 function renderContacts() {
   let contactList = document.getElementById("contact_list");
   contactList.innerHTML = "";
@@ -146,6 +120,18 @@ function renderContacts() {
   }
 }
 
+function deleteInputs() {
+  let inputs = document.querySelectorAll("input");
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+}
+
+function getRandomColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
 function getInitials(name) {
   let words = name.split(" ");
   let initialsArray = words.map(function (word) {
@@ -154,166 +140,4 @@ function getInitials(name) {
   });
   let initials = initialsArray.join("");
   return initials;
-}
-
-function openOverlayAddContact() {
-  let overlayRef = document.getElementById("overlay_add_contacts_background");
-  let overlayCardRef = document.getElementById("overlay_add_contact_card");
-  overlayRef.classList.add("overlay_background");
-  overlayCardRef.classList.add("overlay_add_contact_card");
-  overlayCardRef.style.animation = "slideInFromRightAddContact 0.3s forwards";
-}
-
-function openOverlayEditContact(contactId) {
-  let contact = arrayOfContacts.find((c) => c.id === contactId);
-  let overlayRef = document.getElementById("overlay_add_contacts_background");
-  let overlayEditCardRef = document.getElementById("overlay_edit_contact_card");
-  overlayEditCardRef.innerHTML = getTemplateOfContactEdit(contact);
-  overlayRef.classList.add("overlay_background");
-  overlayEditCardRef.classList.add("overlay_edit_contact_card");
-  overlayEditCardRef.style.animation = "slideInFromRightAddContact 0.3s forwards";
-}
-
-function closeOverlayAddContact() {
-  let overlayRef = document.getElementById("overlay_add_contacts_background");
-  let overlayCardRef = document.getElementById("overlay_add_contact_card");
-  overlayRef.style.backgroundColor = "transparent";
-  overlayCardRef.style.animation = "slideOutToRightAddContact 0.3s forwards";
-  overlayCardRef.addEventListener(
-    "animationend",
-    () => {
-      overlayRef.classList.remove("overlay_background");
-      overlayCardRef.classList.remove("overlay_add_contact_card");
-      overlayCardRef.style.animation = "";
-      overlayRef.style.backgroundColor = "";
-      deleteInputs();
-    },
-    { once: true }
-  );
-}
-
-function overlayContactSuccessfullyCreated() {
-  let overlayRef = document.getElementById("contact_successfully_created");
-  overlayRef.classList.remove("d_none");
-  overlayRef.classList.add("overlay_contact_successfully_created");
-  overlayRef.style.animation = "slideInFromRightContactSuccessfullyCreated 0.3s forwards";
-  setTimeout(() => {
-    overlayRef.style.animation = "slideOutToRightContactSuccessfullyCreated 0.3s forwards";
-  }, 800);
-}
-
-function overlayContactSuccessfullyEdited() {
-  let overlayRef = document.getElementById("contact_successfully_edit");
-  overlayRef.classList.remove("d_none");
-  overlayRef.classList.add("overlay_contact_successfully_created");
-  overlayRef.style.animation = "slideInFromRightContactSuccessfullyCreated 0.3s forwards";
-  setTimeout(() => {
-    overlayRef.style.animation = "slideOutToRightContactSuccessfullyCreated 0.3s forwards";
-  }, 800);
-}
-
-function overlayContactSuccessfullyDelete() {
-  let overlayRef = document.getElementById("contact_successfully_deleted");
-  overlayRef.classList.remove("d_none");
-  overlayRef.classList.add("overlay_contact_successfully_created");
-  overlayRef.style.animation = "slideInFromRightContactSuccessfullyCreated 0.3s forwards";
-  setTimeout(() => {
-    overlayRef.style.animation = "slideOutToRightContactSuccessfullyCreated 0.3s forwards";
-  }, 800);
-}
-
-function setTimeoutSuccessfullyOverlayAddContact() {
-  setTimeout(() => {
-    overlayContactSuccessfullyCreated();
-  }, 200);
-}
-
-function setTimeoutSuccessfullyOverlayEdit() {
-  setTimeout(() => {
-    overlayContactSuccessfullyEdited();
-  }, 200);
-}
-
-function setTimeoutDeleteOverlayContact() {
-  setTimeout(() => {
-    overlayContactSuccessfullyDelete();
-  }, 200);
-}
-
-function closeOverlayEditContact() {
-  let overlayRef = document.getElementById("overlay_add_contacts_background");
-  let overlayCardRef = document.getElementById("overlay_edit_contact_card");
-  overlayRef.style.backgroundColor = "transparent";
-  overlayCardRef.style.animation = "slideOutToRightAddContact 0.3s forwards";
-  overlayCardRef.addEventListener(
-    "animationend",
-    () => {
-      overlayRef.classList.remove("overlay_background");
-      overlayCardRef.classList.remove("overlay_edit_contact_card");
-      overlayCardRef.style.animation = "";
-      overlayRef.style.backgroundColor = "";
-    },
-    { once: true }
-  );
-}
-
-function toggleOverlayContactInfos(index) {
-  let overlay = document.getElementById("overlay_contact_infos");
-  let contactElement = document.getElementById(`contact_${index}`);
-  let contact = arrayOfContacts[index];
-  if (contactElement.classList.contains("contact_active")) {
-    ifContactElementContainsContactActive(overlay, contactElement);
-  } else {
-    removeActiveClassFromContacts();
-    elseContactElementContainsContactActive(overlay, contactElement, contact);
-  }
-}
-
-function removeActiveClassFromContacts() {
-  const activeContacts = document.querySelectorAll(".contact_active");
-  for (let i = 0; i < activeContacts.length; i++) {
-    activeContacts[i].classList.remove("contact_active");
-    activeContacts[i].classList.add("hover_contact_list");
-  }
-}
-
-function ifContactElementContainsContactActive(overlay, contactElement) {
-  contactElement.classList.remove("contact_active");
-  contactElement.classList.add("hover_contact_list");
-  overlay.style.animation = "slideOutToRightContactInfos 0.3s forwards";
-  overlay.addEventListener(
-    "animationend",
-    () => {
-      overlay.classList.add("d_none");
-      overlay.style.animation = "";
-    },
-    { once: true }
-  );
-}
-
-function elseContactElementContainsContactActive(overlay, contactElement, contact) {
-  contactElement.classList.add("contact_active");
-  contactElement.classList.remove("hover_contact_list");
-  overlay.innerHTML = getTemplateOfContactInfo(contact);
-  if (overlay.classList.contains("d_none")) {
-    overlay.classList.remove("d_none");
-    overlay.style.animation = "slideInFromRightContactInfos 0.3s forwards";
-  } else {
-    elseOverlayContactInfosGoOut(overlay, contact);
-  }
-}
-
-function elseOverlayContactInfosGoOut(overlay, contact) {
-  overlay.style.animation = "slideOutToRightContactInfos 0.3s forwards";
-  overlay.addEventListener(
-    "animationend",
-    () => {
-      overlay.classList.add("d_none");
-      overlay.style.animation = "";
-      overlay.innerHTML = getTemplateOfContactInfo(contact);
-      overlay.classList.remove("d_none");
-      overlay.style.animation = "slideInFromRightContactInfos 0.3s forwards";
-    },
-    { once: true }
-  );
 }
