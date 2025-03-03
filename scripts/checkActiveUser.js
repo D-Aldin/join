fe831e5eb64da29282345f5da410fd4522c75929
RobbyR98 <robbyrunge@yahoo.com>
2025-03-03 20:27:33 +0100

    fixed bug with the logout funtionality and delete the text by date_deadline and text_deadline

--- scripts/checkActiveUser.js
+++ scripts/checkActiveUser.js
@@ -4,13 +4,13 @@ function checkUser() {
   if (isGuest === "true" || userId) {
     return;
   }
-  window.location.replace("../login.html");
+  window.location.replace("./login.html");
 }
 
 function logout() {
   localStorage.removeItem("userId");
   localStorage.removeItem("isGuest");
-  window.location.replace("../login.html");
+  window.location.replace("./login.html");
   if (window.history && window.history.pushState) {
     window.history.pushState(null, "", window.location.href);
     window.onpopstate = function () {
