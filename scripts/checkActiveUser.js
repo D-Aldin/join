d265fc49f34bdb56070efd5a89874a4c96164265
RobbyR98 <robbyrunge@yahoo.com>
2025-03-08 19:28:59 +0100

    fixed logout bug

--- scripts/checkActiveUser.js
+++ scripts/checkActiveUser.js
@@ -4,13 +4,13 @@ function checkUser() {
   if (isGuest === "true" || userId) {
     return;
   }
-  window.location.replace("./index.html");
+  window.location.replace("index.html");
 }
 
 function logout() {
   localStorage.removeItem("userId");
   localStorage.removeItem("isGuest");
-  window.location.replace("./index.html");
+  window.location.replace("index.html");
   if (window.history && window.history.pushState) {
     window.history.pushState(null, "", window.location.href);
     window.onpopstate = function () {
