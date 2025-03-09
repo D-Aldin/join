e04a4d9781bc433eabf9ddfbf5f442c0caa10084
D-Aldin <dobricaldin@gmail.com>
2025-03-09 18:29:33 +0100

    add JSDoc for drop_down_menu.js

--- scripts/drop_down_menu.js
+++ scripts/drop_down_menu.js
@@ -1,6 +1,9 @@
-const addTaskWindow = document.querySelector("#add_task_box");
-const contentWindow = document.querySelector("#box");
-
+/**
+ * Toggles the visibility of the content menu. If the menu is currently displayed inline,
+ * it will be hidden; otherwise, it will be shown. It also updates the arrow icon accordingly.
+ *
+ * @function openMenu
+ */
 function openMenu() {
   const contentMenu = document.querySelector(".content");
   if (contentMenu.style.display === "inline") {
@@ -18,6 +21,12 @@ function openMenu() {
   }
 }
 
+/**
+ * Toggles the visibility of the "contentSectionAddTask" menu. If the menu is currently displayed inline,
+ * it will be hidden; otherwise, it will be shown. It also updates the arrow icon accordingly.
+ *
+ * @function openMenuSectionAddTask
+ */
 function openMenuSectionAddTask() {
   const contentMenu = document.querySelector(".contentSectionAddTask");
   if (contentMenu.style.display === "inline") {
