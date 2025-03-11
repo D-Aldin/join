0703dcf1c57fb13e264b1481df62c7585b0bb9c3
Simib92 <simib92@hotmail.ch>
2025-03-11 20:22:55 +0100

    css: all align left, new function add_task_template

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -73,19 +73,6 @@ function pushContactsToSelectField() {
  * @param {number} i - The index of the contact in the array.
  * @returns {void}
  */
-function renderContactTemplate(contactlist, currentContact, i) {
-  contactlist.innerHTML += /*html*/ `
-            <div onclick="addContact(${i})" id="contact${i}" class="contactlist">
-                <div class="flex_gap_16">
-                    <div class="contact-img-cyrcle" style="background-color: ${currentContact.color}">${returnFirstLetter(currentContact.name)}</div>
-                    ${currentContact.name}
-                </div>
-                <div id="contact-checkbox${i}">
-                    <img src="assets/icons/addTask/box.svg" alt="">
-                </div>
-            </div>
-        `;
-}
 
 /**
  * Filters and renders contacts based on search input.
