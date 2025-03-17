02608d8eb9917e432216e6b05ae3915bf7870e69
D-Aldin <dobricaldin@gmail.com>
2025-03-17 22:31:32 +0100

    retrieve changes made post-delivery

--- scripts/board.js
+++ scripts/board.js
@@ -54,8 +54,15 @@ function highlightDropPoint(dragevent) {
  */
 function dropPoint(event) {
   event.preventDefault();
+  let newStatus;
+  let data = event.dataTransfer.getData("text");
+  let draggedElement = document.getElementById(data);
+  if (event.target.classList.contains("card")) {
+    const valueForNewStatus = event.target.closest("section").firstElementChild.nextElementSibling.nextElementSibling.id;
+    event.target.before(draggedElement);
+    newStatus = valueForNewStatus;
+  }
   if (event.target.id == "toDo" || event.target.id == "progress" || event.target.id == "feedback" || event.target.id == "done") {
-    let data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
     let newStatus = event.target.id;
     let statusUpdate = {
