29318f340d329a6d47307e782c6f77279f81613f
RobbyR98 <robbyrunge@yahoo.com>
2025-03-20 10:04:32 +0100

    fix bug by clicking in the checkbox in the contact list its close the wrapper by add task and update some design elements

--- scripts/add_task_template.js
+++ scripts/add_task_template.js
@@ -1,13 +1,11 @@
 function renderContactTemplate(contactlist, currentContact, i) {
-    contactlist.innerHTML += /*html*/ `
-              <div onclick="addContact(${i})" id="contact${i}" class="contactlist">
-                  <div class="flex_gap_16">
-                      <div class="contact-img-cyrcle" style="background-color: ${currentContact.color}">${returnFirstLetter(currentContact.name)}</div>
-                      ${currentContact.name}
-                  </div>
-                  <div id="contact-checkbox${i}">
-                      <img src="assets/icons/addTask/box.svg" alt="">
-                  </div>
-              </div>
-          `;
-  }
+  contactlist.innerHTML += `<div id="contact${i}" class="contactlist" onclick="addContact(${i}); event.stopPropagation();">
+    <div class="flex_gap_16">
+        <div class="contact-img-cyrcle" style="background-color: ${currentContact.color}">${returnFirstLetter(currentContact.name)}</div>
+        ${currentContact.name}
+    </div>
+    <div id="contact-checkbox${i}" onclick="addContact(${i}); event.stopPropagation();">
+        <img src="assets/icons/addTask/box.svg" alt="">
+    </div>
+</div>`;
+}
